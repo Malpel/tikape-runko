cd6d95764229dd47d165a73d862e962b159c626c
Malpel <m.a.pelkonen2@gmail.com>
2017-10-23 11:03:21 +0300

    luotu html-sivut ja niiden GET

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -2,24 +2,43 @@ package tikape.runko;
 
 import java.util.HashMap;
 import spark.ModelAndView;
+import spark.Spark;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
 import tikape.runko.database.AnnosDao;
+import tikape.runko.database.AnnosRaakaAineDao;
+import tikape.runko.database.RaakaAineDao;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:db/smoothiet.db");
-        
 
-        AnnosDao opiskelijaDao = new AnnosDao(database);
-        
-        
-        
-        
-        
-        
+        AnnosDao annokset = new AnnosDao(database);
+        RaakaAineDao raakaAineet = new RaakaAineDao(database);
+        AnnosRaakaAineDao annosRaakaAine = new AnnosRaakaAineDao(database);
+
+        Spark.get("/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("smoothiet", annokset.findAll());
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.get("/ainekset", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("raakaAineet", raakaAineet.findAll());
+
+            return new ModelAndView(map, "ainekset");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.get("/smoothiet", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("smoothiet", annokset.findAll());
+            
+            return new ModelAndView(map, "smoothiet");
+        }, new ThymeleafTemplateEngine());
         /*
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -41,6 +60,6 @@ public class Main {
 
             return new ModelAndView(map, "opiskelija");
         }, new ThymeleafTemplateEngine());
-        */
+         */
     }
 }
